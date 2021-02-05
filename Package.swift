0309bf426542c53f5913d8e72e5a35774a02a800
Julian Panucci <julian.panucci@underarmour.com>
2021-02-05 13:55:37 -0600

    Fix package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FrameworkB",
-			exclude: ["Sources/FrameworkB/Info.plist"]
+			exclude: ["Sources/FrameworkB/Info.plist"],
             dependencies: []),
         .testTarget(
             name: "FrameworkBTests",
