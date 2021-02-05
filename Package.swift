489004b34632879ee1359ea0ff1685f8d63e1a05
Julian Panucci <julian.panucci@underarmour.com>
2021-02-05 14:34:24 -0600

    Stuff

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FrameworkB",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -20,9 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FrameworkB",
-            dependencies: []),
+            path: "FrameworkB",
+            exclude: ["Info.plist", "FrameworkB.h"]),
         .testTarget(
             name: "FrameworkBTests",
             dependencies: ["FrameworkB"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
