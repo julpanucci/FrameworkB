59acbe15ab4b781ac78dd24a211ab5c27bb2fd16
Julian Panucci <julian.panucci@underarmour.com>
2021-02-05 13:56:31 -0600

    Fix package agin

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FrameworkB",
+			dependencies: []),
 			exclude: ["Sources/FrameworkB/Info.plist"],
-            dependencies: []),
         .testTarget(
             name: "FrameworkBTests",
             dependencies: ["FrameworkB"]),
