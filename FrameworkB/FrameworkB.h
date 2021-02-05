aa5ae7c97768f0e749de8a37015ef4fc6ba83580
Julian Panucci <julian.panucci@underarmour.com>
2021-02-05 13:35:50 -0600

    Initial Commit

---
+++ FrameworkB/FrameworkB.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkB.h
+//  FrameworkB
+//
+//  Created by Julian Panucci on 2/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkB.
+FOUNDATION_EXPORT double FrameworkBVersionNumber;
+
+//! Project version string for FrameworkB.
+FOUNDATION_EXPORT const unsigned char FrameworkBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkB/PublicHeader.h>
+
+
